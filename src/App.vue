a199bffc1061148ca46793dd10c2e913f9532da6
Odzen <jsebastian.va@gmail.com>
2022-10-25 02:26:34 -0500

    Fetching data from API

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,71 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <div v-if="!clients.length" class="loading">Loading Clients...</div>
+
+    <div class="clients" v-for="client in clients" :key="client.name">
+      <h2>{{ client.name }}</h2>
+    </div>
+
+    <div v-if="!files.length" class="loading">Loading Files...</div>
+    <div class="files" v-for="file in files" :key="file.name">
+      <h2>{{ file.name }}</h2>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue';
+
+import axios from 'axios';
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld,
+  data() {
+    return {
+      bottom: false,
+      clients: [],
+      files: [],
+    };
+  },
+  watch: {
+    files() {
+      setTimeout(() => {
+        this.getFiles();
+      }, 2000);
+    },
+
+    clients() {
+      setTimeout(() => {
+        this.getClients();
+      }, 2000);
+    },
+  },
+  created() {
+    this.getFiles();
+    this.getClients();
+  },
+  methods: {
+
+    getFiles() {
+      axios.get('http://localhost:3030/files').then((response) => {
+        const filesApi = response.data;
+        this.files = filesApi;
+      }).catch((error) => {
+        // eslint-disable-next-line no-console
+        console.error(error);
+      });
+    },
+
+    getClients() {
+      axios.get('http://localhost:3030/clients').then((response) => {
+        const clientsApi = response.data;
+        this.clients = clientsApi;
+      }).catch((error) => {
+        // eslint-disable-next-line no-console
+        console.error(error);
+      });
+    },
   },
 };
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
